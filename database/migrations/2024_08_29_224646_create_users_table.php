8d25823c5d321a27db8ad2efd29f9c48f4f94746
alejandroGleles <alejandro.leles@estudante.iftm.edu.br>
2024-08-29 20:05:24 -0300

    feat: create table user

--- database/migrations/2024_08_29_224646_create_users_table.php
+++ database/migrations/2024_08_29_224646_create_users_table.php
@@ -15,7 +15,26 @@ return new class extends Migration
 		Schema::create('users', function(Blueprint $table) {
             $table->increments('id');
 
+			//people data
+			$table->string('cpf, 11')->unique()->nullable();
+			$table->string('name',50);
+			$table->char('phone',11);
+			$table->date('birth')->nullable();
+			$table->char('gender',1)->nullable();
+			$table->text('notas')->nullable();
+
+			//auth data
+			$table->string('email',80)->unique();
+			$table->string('password',254)->nullable();
+
+			//permission
+			$table->string('status',0)->default('active');
+			$table->string('permission',0)->default('app.user');
+
+			$table->rememberToken();
             $table->timestamps();
+			$table->softDeletes();
+
 		});
 	}
 
@@ -26,6 +45,9 @@ return new class extends Migration
 	 */
 	public function down()
 	{
+		Schema::table('users', function(Blueprint $table) {
+			
+		});
 		Schema::drop('users');
 	}
 };
